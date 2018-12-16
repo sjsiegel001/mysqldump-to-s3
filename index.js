a78a71ce9c439c0567fb2cf7802d37770200004b
Steve <steve@Steves-Air.home>
2018-12-16 13:49:13 -0500

    fixed promises

--- index.js
+++ index.js
@@ -15,44 +15,74 @@ const db_datestringed_name = 'db_backup_' + moment().format('_MM-DD-YYYY_hhmmA')
 let s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 exports.handler = function(event, context, callback) {
-
-  new Promise(function(resolve, reject) {
-
-    msd({
-        connection: {
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME
-        },
-        dumpToFile: '/tmp/' + db_datestringed_name + '.sql',
-    }).then(resolve(1))
-
-  }).then(function(result) {
-
-
-    tar.c(
-      {
-        gzip: true,
-        file: '/tmp/' + db_datestringed_name + '.tgz'
-      },
-      ['/tmp/' + db_datestringed_name + '.sql']
-    ).then(_ => { console.log('tarball created'); })
-
-  }).then(function(result) {
-
-    fs.readFile('/tmp/' + db_datestringed_name + '.tgz', function(err, data) {
-      if (err) throw err;
-
-      var base64data = new Buffer.from(data, 'binary');
-      s3.putObject({
-        Bucket: process.env.S3_PATH,
-        Key: db_datestringed_name + '.tgz',
-        Body: base64data,
-      },function (resp) {
-
-        console.log('Successfully uploaded backup to s3.');
-      });
-    });
-  });
+  grabSqlDmup()
+  .then(makeTarball)
+  .then(offloadToS3);
 }
+
+var grabSqlDmup = function() {
+  return new Promise(
+    function(resolve, reject) {
+
+      msd({
+          connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+          },
+          dumpToFile: '/tmp/' + db_datestringed_name + '.sql',
+      }).then(_ => {
+          console.log("dumped .sql file");
+          resolve();
+        }
+      );
+
+    }
+  );
+};
+
+var makeTarball = function() {
+  return new Promise(
+    function(resolve, reject) {
+
+      tar.c(
+        {
+          gzip: true,
+          file: '/tmp/' + db_datestringed_name + '.tgz'
+        },
+        ['/tmp/' + db_datestringed_name + '.sql']
+      ).then(_ => {
+          console.log("created tarball");
+          resolve();
+        }
+      );
+
+    }
+  );
+};
+
+var offloadToS3 = function() {
+  return new Promise(
+    function(resolve, reject) {
+
+      fs.readFile('/tmp/' + db_datestringed_name + '.tgz', function(err, data) {
+        if (err) throw err;
+
+        var base64data = new Buffer.from(data, 'binary');
+        s3.putObject({
+          Bucket: process.env.S3_PATH,
+          Key: db_datestringed_name + '.tgz',
+          Body: base64data,
+        },function (resp) {
+          if(resp === null) {
+            console.log('Successfully uploaded backup to s3.');
+          } else {
+            console.log(resp);
+          }
+        });
+      });
+
+    }
+  );
+};
